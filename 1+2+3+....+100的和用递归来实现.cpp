80c0c0df9b7a848f166c39357633e7a1fb9e4d73
byk <38996147+bailongmahaha@users.noreply.github.com>
2018-05-10 14:04:17 +0800

    Update 1+2+3+....+100的和用递归来实现.cpp

--- 1+2+3+....+100的和用递归来实现.cpp
+++ 1+2+3+....+100的和用递归来实现.cpp
@@ -5,13 +5,12 @@ long sum(int n)
 	if (1 == n)
 		return 1;
 	else
-		return n + sum(n-1);
+		return n + sum(n-1);		//递归调用
 }
 
 int main(void)
 {
-	printf("%ld\n", sum(100));
-
+	printf("%ld\n", sum(100));		
 	return 0;
 }
 
